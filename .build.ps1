4ad4e8929fe27cd657ef46cbbe556b096b2e5b41
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-02-22 23:06:35 +0100

    updated psbuildtasks

--- .build.ps1
+++ .build.ps1
@@ -1,2 +1,26 @@
+param(
+	[string] $NuGetApiKey = $env:nuget_apikey,
+
+	# Overwrite published versions.
+	[switch] $ForcePublish,
+
+    # Add doc templates for new command.
+	[switch] $ForceDocInit,
+
+	# Version suffix to prereleases.
+	[int] $BuildNumber,
+
+    # Use specific branch from PsBuildTasks.
+    [string] $PsBuildTaskBranch
+)
+
 $ModuleName = 'PsVsSolution'
+
 . ./tasks/Build.Tasks.ps1
+. ./tasks/PsBuild.Tasks.ps1
+
+task InstallBuildDependencies {
+    Install-Module platyPs -ErrorAction Stop
+}
+task InstallTestDependencies {}
+task InstallReleaseDependencies -Jobs {}
